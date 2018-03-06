730623c024958d8cdf420feb7e4b07b4c9612efa
Jason Warta <jasonwarta@gmail.com>
2018-03-06 09:57:07 -0900

    added basic prompt functionality to set up config on first run

--- index.js
+++ index.js
@@ -58,332 +58,334 @@ const statelessPrompt = new Radio({
 	],
 });
 
-const setupConfig = async (configDir) => {
-	if(!fs.pathExistsSync(configDir)) {
-		const conf = {};
-		console.log('It looks like this is the first time you have run compcreate.');
-		await setupPrompt.run().then( async (answer) => {
-			if(answer == 'Personalize') {
-				conf.createDirectory = await dirPrompt.run().then( (answer) => {
-					return answer == 'Yes' ? true : false;
-				});
-
-				conf.createIndex = await (!conf.createDirectory ? false : indexPrompt.run().then( (answer) => {
-					return answer == 'Yes' ? true : false;
-				}));
-
-				conf.createScss = await scssPrompt.run().then( (answer) => {
-					return answer == 'Yes' ? true : false;
-				});
-
-				conf.createStateless = await statelessPrompt.run().then( (answer) => {
-					return answer == 'Yes' ? true : false;
-				});
-				
-				fs.writeJsonSync(configDir, conf);
-				return require(configDir);
-			}
-			else {
-				fs.copySync(`${packageDirectory}config.json`, configDir);
-				return require(configDir);
-			}
-		});
-	}
-	else
-		return require(configDir);
-};
-
-let defaultDir = '';
-if(!fs.pathExistsSync(configDir)) {
-	setupConfig();
-	defaultDir = './config.json';
-};
-
-const config = require(defaultDir != '' ? defaultDir : configDir);
-const version = require(`${packageDirectory}package.json`).version;
-const ArgumentParser = require('argparse').ArgumentParser;
-
-const parseBool = (value) => {
-	if(['yes', 'true', 't', 'y', '1'].indexOf(value.toLowerCase()) > -1)
-		return true;
-	else if(['no', 'false', 'f', 'n', '0'].indexOf(value.toLowerCase()) > -1)
-		return false;
-	else {
-		// eslint-disable-next-line no-console
-		console.error(`Could not read value given as boolean: '${value}'`);
-		process.exit(1);
-	}
-};
-
-const parser = new ArgumentParser({
-	version,
-	addHelp: true,
-	description: 'Compcreate is a command line tool for creating React components easily.',
-});
-
-parser.addArgument(['names'], {
-	help: 'A list of desired component names.',
-	nargs: '*',
-});
-
-if(config) {
-parser.addArgument(['-d', '--directory'], {
-	help: `Creates a new directory for component files.`,
-	defaultValue: config.createDirectory,
-	nargs: '?',
-	const: true,
-	type: parseBool,
-});
-
-parser.addArgument(['-i', '--index'], {
-	help: `Creates an index.js file for easier importing from directory.`,
-	defaultValue: config.createIndex,
-	nargs: '?',
-	const: true,
-	type: parseBool,
-});
-
-parser.addArgument(['-s', '--stateless'], {
-	help: `Creates a stateless component that is included inside main component.`,
-	defaultValue: config.createStateless,
-	nargs: '?',
-	const: true,
-	type: parseBool,
-});
-
-parser.addArgument(['-c', '--scss'], {
-	help: `Creates a SCSS file for custom component styles.`,
-	defaultValue: config.createScss,
-	nargs: '?',
-	const: true,
-	type: parseBool,
-});
-}
-parser.addArgument(['--save-config'], {
-	help: `Saves configuration used in command as new default.`,
-	action: 'storeTrue',
-});
-
-parser.addArgument(['--config'], {
-	help: `Step through the process of setting up a config file.`,
-	action: 'storeTrue',
-});
-
-const args = parser.parseArgs();
-
-// If flags were set but given no value
-if(args.directory === null)
-	args.directory = true;
-
-if(args.index === null)
-	args.index = true;
-
-if(args.stateless === null)
-	args.stateless = true;
-
-if(args.scss === null)
-	args.scss = true;
-
-const templateFile = 'template.js';
-const statelessFile = 'template.stateless.js';
-const indexFile = 'index.js';
-
-const backtracker = {};
-
-if(args['save_config']) {
-	config.createDirectory = args.directory;
-	config.createIndex = args.index;
-	config.createStateless = args.stateless;
-	config.createScss = args.scss;
-
-	fs.writeJsonSync(configDir, config);
-
-	// eslint-disable-next-line no-console
-	console.log('New configuration saved.');
-}
-
-if(args['config']) {
-	async () => {
-		console.log('starting config setup');
-		await setupConfig();
-	}
-
-	console.log('config');
-	process.exit(1);;
-};
-
-if(!fs.pathExistsSync(configDir)) 
-	console.log('you must run compcreate --config');
-
-if(!args.names.length) {
-	if(!args['save_config'])
-		parser.printHelp();
-
-	process.exit(1);
-}
-
-const getFileNameFromPath = (dirPath) => {
-	const splitPath = dirPath.split(path.sep);
-
-	if(splitPath.length > 0)
-		return splitPath[splitPath.length - 1];
-	else
-		return '';
-};
-
-const getBaseDirectory = (dirPath) => {
-	const pwd = process.cwd() + path.sep;
-	const splitPath = dirPath.split(path.sep);
-
-	if(splitPath.length > 0) {
-		const baseChunks = splitPath.slice(0, splitPath.length - 1);
-		return pwd + baseChunks.join(path.sep);
-	}
-	else
-		return pwd;
-};
-
-// fs-extra emptyDir function does not seem to work...
-const directoryIsEmpty = (dir) => {
-	return new Promise((resolve, reject) => {
-		fs.readdir(dir, (err, items) => {
-			if(err)
-				reject(err);
-			else
-				resolve(items.length === 0);
-		});
-	});
-};
-
-const replaceInFile = (filePath, params) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(filePath, 'utf8', (err, data) => {
-			if(err)
-				reject(err);
-			else {
-				params.forEach((replaceParams) => {
-					const reg = new RegExp(replaceParams[0], 'g');
-					data = data.replace(reg, replaceParams[1]);
-				});
-
-				fs.writeFile(filePath, data, (err) => {
-					if(err)
-						reject(err);
-					else
-						resolve();
-				});
-			}
-		});
-	});
-};
-
-const cleanupAndExit = () => {
-	for(const key in backtracker) {
-		if(Object.hasOwnProperty.call(backtracker, key)) {
-			const tracker = backtracker[key];
-
-			tracker.filesCreated.forEach((file) => {
-				fs.removeSync(file);
+const setupConfig = () => {
+	const conf = {};
+	return setupPrompt.run()
+	.then( async (answer) => {
+		if(answer == 'Personalize') {
+			conf.createDirectory = await dirPrompt.run().then( (answer) => {
+				return answer == 'Yes' ? true : false;
 			});
 
-			tracker.directoriesCreated.forEach((file) => {
-				fs.removeSync(file);
+			conf.createIndex = await (!conf.createDirectory ? false : indexPrompt.run().then( (answer) => {
+				return answer == 'Yes' ? true : false;
+			}));
+
+			conf.createScss = await scssPrompt.run().then( (answer) => {
+				return answer == 'Yes' ? true : false;
 			});
+
+			conf.createStateless = await statelessPrompt.run().then( (answer) => {
+				return answer == 'Yes' ? true : false;
+			});
+
+			fs.writeJsonSync(configDir, conf);
 		}
-	}
+		else
+			fs.copySync(`${packageDirectory}config.json`, configDir);
 
-	process.exit(1);
+		console.log(`Saved configuration in ${configDir}`);
+	}).catch( (err) => {
+		console.log(err);
+	});
 };
 
-const createReactComponentFiles = async (dirPath) => {
-	backtracker[dirPath] = {
-		filesCreated: [],
-		directoriesCreated: [],
+if(!fs.pathExistsSync(configDir))
+	setupConfig();
+else {
+	const config = require(configDir);
+	const version = require(`${packageDirectory}package.json`).version;
+	const ArgumentParser = require('argparse').ArgumentParser;
+
+	const parseBool = (value) => {
+		if(['yes', 'true', 't', 'y', '1'].indexOf(value.toLowerCase()) > -1)
+			return true;
+		else if(['no', 'false', 'f', 'n', '0'].indexOf(value.toLowerCase()) > -1)
+			return false;
+		else {
+			// eslint-disable-next-line no-console
+			console.error(`Could not read value given as boolean: '${value}'`);
+			process.exit(1);
+		}
 	};
 
-	const filename = getFileNameFromPath(dirPath);
-	let baseDirectory = getBaseDirectory(dirPath);
-	const directoryExists = await fs.pathExists(dirPath);
+	const parser = new ArgumentParser({
+		version,
+		addHelp: true,
+		description: 'Compcreate is a command line tool for creating React components easily.',
+	});
 
-	if(args.directory) {
-		if(!directoryExists) {
-			try {
-				await fs.ensureDir(dirPath);
-				backtracker[dirPath].directoriesCreated.push(dirPath);
-			}
-			catch(err) {
-				// eslint-disable-next-line no-console
-				console.error(`Could not create directory ${dirPath}: ${err}`);
+	parser.addArgument(['names'], {
+		help: 'A list of desired component names.',
+		nargs: '*',
+	});
 
-				cleanupAndExit();
+	parser.addArgument(['-d', '--directory'], {
+		help: `Creates a new directory for component files.`,
+		defaultValue: config.createDirectory,
+		nargs: '?',
+		const: true,
+		type: parseBool,
+	});
+
+	parser.addArgument(['-i', '--index'], {
+		help: `Creates an index.js file for easier importing from directory.`,
+		defaultValue: config.createIndex,
+		nargs: '?',
+		const: true,
+		type: parseBool,
+	});
+
+	parser.addArgument(['-s', '--stateless'], {
+		help: `Creates a stateless component that is included inside main component.`,
+		defaultValue: config.createStateless,
+		nargs: '?',
+		const: true,
+		type: parseBool,
+	});
+
+	parser.addArgument(['-c', '--scss'], {
+		help: `Creates a SCSS file for custom component styles.`,
+		defaultValue: config.createScss,
+		nargs: '?',
+		const: true,
+		type: parseBool,
+	});
+	
+	parser.addArgument(['--save-config'], {
+		help: `Saves configuration used in command as new default.`,
+		action: 'storeTrue',
+	});
+
+	const args = parser.parseArgs();
+
+	// If flags were set but given no value
+	if(args.directory === null)
+		args.directory = true;
+
+	if(args.index === null)
+		args.index = true;
+
+	if(args.stateless === null)
+		args.stateless = true;
+
+	if(args.scss === null)
+		args.scss = true;
+
+	const templateFile = 'template.js';
+	const statelessFile = 'template.stateless.js';
+	const indexFile = 'index.js';
+
+	const backtracker = {};
+
+	if(args['save_config']) {
+		config.createDirectory = args.directory;
+		config.createIndex = args.index;
+		config.createStateless = args.stateless;
+		config.createScss = args.scss;
+
+		fs.writeJsonSync(configDir, config);
+
+		// eslint-disable-next-line no-console
+		console.log('New configuration saved.');
+	}
+
+	if(!args.names.length) {
+		if(!args['save_config'])
+			parser.printHelp();
+
+		process.exit(1);
+	}
+
+	const getFileNameFromPath = (dirPath) => {
+		const splitPath = dirPath.split(path.sep);
+
+		if(splitPath.length > 0)
+			return splitPath[splitPath.length - 1];
+		else
+			return '';
+	};
+
+	const getBaseDirectory = (dirPath) => {
+		const pwd = process.cwd() + path.sep;
+		const splitPath = dirPath.split(path.sep);
+
+		if(splitPath.length > 0) {
+			const baseChunks = splitPath.slice(0, splitPath.length - 1);
+			return pwd + baseChunks.join(path.sep);
+		}
+		else
+			return pwd;
+	};
+
+	// fs-extra emptyDir function does not seem to work...
+	const directoryIsEmpty = (dir) => {
+		return new Promise((resolve, reject) => {
+			fs.readdir(dir, (err, items) => {
+				if(err)
+					reject(err);
+				else
+					resolve(items.length === 0);
+			});
+		});
+	};
+
+	const replaceInFile = (filePath, params) => {
+		return new Promise((resolve, reject) => {
+			fs.readFile(filePath, 'utf8', (err, data) => {
+				if(err)
+					reject(err);
+				else {
+					params.forEach((replaceParams) => {
+						const reg = new RegExp(replaceParams[0], 'g');
+						data = data.replace(reg, replaceParams[1]);
+					});
+
+					fs.writeFile(filePath, data, (err) => {
+						if(err)
+							reject(err);
+						else
+							resolve();
+					});
+				}
+			});
+		});
+	};
+
+	const cleanupAndExit = () => {
+		for(const key in backtracker) {
+			if(Object.hasOwnProperty.call(backtracker, key)) {
+				const tracker = backtracker[key];
+
+				tracker.filesCreated.forEach((file) => {
+					fs.removeSync(file);
+				});
+
+				tracker.directoriesCreated.forEach((file) => {
+					fs.removeSync(file);
+				});
 			}
 		}
-		else {
-			try {
-				const empty = await directoryIsEmpty(dirPath);
 
-				if(!empty) {
+		process.exit(1);
+	};
+
+	const createReactComponentFiles = async (dirPath) => {
+		backtracker[dirPath] = {
+			filesCreated: [],
+			directoriesCreated: [],
+		};
+
+		const filename = getFileNameFromPath(dirPath);
+		let baseDirectory = getBaseDirectory(dirPath);
+		const directoryExists = await fs.pathExists(dirPath);
+
+		if(args.directory) {
+			if(!directoryExists) {
+				try {
+					await fs.ensureDir(dirPath);
+					backtracker[dirPath].directoriesCreated.push(dirPath);
+				}
+				catch(err) {
 					// eslint-disable-next-line no-console
-					console.error(`Directory ${dirPath} not empty.`);
+					console.error(`Could not create directory ${dirPath}: ${err}`);
 
 					cleanupAndExit();
 				}
 			}
+			else {
+				try {
+					const empty = await directoryIsEmpty(dirPath);
+
+					if(!empty) {
+						// eslint-disable-next-line no-console
+						console.error(`Directory ${dirPath} not empty.`);
+
+						cleanupAndExit();
+					}
+				}
+				catch(err) {
+					// eslint-disable-next-line no-console
+					console.error(`Could not verify that directory ${dirPath} is empty: ${err}`);
+
+					cleanupAndExit();
+				}
+			}
+
+			baseDirectory += path.sep + filename;
+		}
+
+		const mainFilePath = `${baseDirectory + path.sep + filename}.js`;
+		const template = `${packageDirectory}template/${templateFile}`;
+
+		try {
+			await fs.copy(template, mainFilePath);
+			backtracker[dirPath].filesCreated.push(mainFilePath);
+
+			try {
+				await replaceInFile(mainFilePath, [
+					['CLASSNAME', filename],
+					[
+						'STATELESS',
+						args.stateless ?
+							`\nimport ${filename}Stateless from './${filename}.stateless';` : '',
+					],
+					[
+						'RETURNCONTENTS',
+						args.stateless ?
+							`<${filename}Stateless />;` : '<div></div>',
+					],
+				]);
+			}
 			catch(err) {
 				// eslint-disable-next-line no-console
-				console.error(`Could not verify that directory ${dirPath} is empty: ${err}`);
+				console.error(`Could not replace contents of template ${mainFilePath}: ${err}`);
 
 				cleanupAndExit();
 			}
 		}
-
-		baseDirectory += path.sep + filename;
-	}
-
-	const mainFilePath = `${baseDirectory + path.sep + filename}.js`;
-	const template = `${packageDirectory}template/${templateFile}`;
-
-	try {
-		await fs.copy(template, mainFilePath);
-		backtracker[dirPath].filesCreated.push(mainFilePath);
-
-		try {
-			await replaceInFile(mainFilePath, [
-				['CLASSNAME', filename],
-				[
-					'STATELESS',
-					args.stateless ?
-						`\nimport ${filename}Stateless from './${filename}.stateless';` : '',
-				],
-				[
-					'RETURNCONTENTS',
-					args.stateless ?
-						`<${filename}Stateless />;` : '<div></div>',
-				],
-			]);
-		}
 		catch(err) {
 			// eslint-disable-next-line no-console
-			console.error(`Could not replace contents of template ${mainFilePath}: ${err}`);
+			console.error(`Could not copy template ${template}: ${err}`);
 
 			cleanupAndExit();
 		}
-	}
-	catch(err) {
-		// eslint-disable-next-line no-console
-		console.error(`Could not copy template ${template}: ${err}`);
 
-		cleanupAndExit();
-	}
+		if(args.index) {
+			const filePath = `${baseDirectory + path.sep}index.js`;
+			const template = `${packageDirectory}template/${indexFile}`;
 
-	if(args.index) {
-		const filePath = `${baseDirectory + path.sep}index.js`;
-		const template = `${packageDirectory}template/${indexFile}`;
+			if(!args.directory)
+				// eslint-disable-next-line no-console
+				console.warn(`Component not being created with directory. Did not create ${filePath}.`);
+			else {
+				try {
+					await fs.copy(template, filePath);
+					backtracker[dirPath].filesCreated.push(filePath);
 
-		if(!args.directory)
-			// eslint-disable-next-line no-console
-			console.warn(`Component not being created with directory. Did not create ${filePath}.`);
-		else {
+					try {
+						await replaceInFile(filePath, [['CLASSNAME', filename]]);
+					}
+					catch(err) {
+						// eslint-disable-next-line no-console
+						console.error(`Could not replace contents of template ${filePath}: ${err}`);
+
+						cleanupAndExit();
+					}
+				}
+				catch(err) {
+					// eslint-disable-next-line no-console
+					console.error(`Could not copy template ${template}: ${err}`);
+
+					cleanupAndExit();
+				}
+			}
+		}
+
+		if(args.stateless) {
+			const filePath = `${baseDirectory + path.sep + filename}.stateless.js`;
+			const template = `${packageDirectory}template/${statelessFile}`;
+
 			try {
 				await fs.copy(template, filePath);
 				backtracker[dirPath].filesCreated.push(filePath);
@@ -405,60 +407,34 @@ const createReactComponentFiles = async (dirPath) => {
 				cleanupAndExit();
 			}
 		}
-	}
 
-	if(args.stateless) {
-		const filePath = `${baseDirectory + path.sep + filename}.stateless.js`;
-		const template = `${packageDirectory}template/${statelessFile}`;
+		if(args.scss) {
+			const filePath = `${baseDirectory + path.sep + filename}.scss`;
 
-		try {
-			await fs.copy(template, filePath);
-			backtracker[dirPath].filesCreated.push(filePath);
+			const exists = await fs.pathExists(filePath);
 
-			try {
-				await replaceInFile(filePath, [['CLASSNAME', filename]]);
-			}
-			catch(err) {
-				// eslint-disable-next-line no-console
-				console.error(`Could not replace contents of template ${filePath}: ${err}`);
+			if(!exists) {
+				try {
+					await fs.ensureFile(filePath);
+					backtracker[dirPath].filesCreated.push(filePath);
+				}
+				catch(err) {
+					// eslint-disable-next-line no-console
+					console.error(`Could not create ${filePath}: ${err}`);
 
-				cleanupAndExit();
-			}
-		}
-		catch(err) {
-			// eslint-disable-next-line no-console
-			console.error(`Could not copy template ${template}: ${err}`);
-
-			cleanupAndExit();
-		}
-	}
-
-	if(args.scss) {
-		const filePath = `${baseDirectory + path.sep + filename}.scss`;
-
-		const exists = await fs.pathExists(filePath);
-
-		if(!exists) {
-			try {
-				await fs.ensureFile(filePath);
-				backtracker[dirPath].filesCreated.push(filePath);
-			}
-			catch(err) {
-				// eslint-disable-next-line no-console
-				console.error(`Could not create ${filePath}: ${err}`);
-
-				cleanupAndExit();
+					cleanupAndExit();
+				}
 			}
 		}
-	}
-	
-	// eslint-disable-next-line no-console
-	console.log('Successfully created files:');
-	backtracker[dirPath].filesCreated.forEach((file) => {
+		
 		// eslint-disable-next-line no-console
-		console.log(file);
-	});
-};
+		console.log('Successfully created files:');
+		backtracker[dirPath].filesCreated.forEach((file) => {
+			// eslint-disable-next-line no-console
+			console.log(file);
+		});
+	};
 
-for(const dirPath of args.names)
-	createReactComponentFiles(dirPath);
+	for(const dirPath of args.names)
+		createReactComponentFiles(dirPath);
+}
